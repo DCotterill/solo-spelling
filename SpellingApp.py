f72f4a76518515c04f7da26b8b10c4d2c13281a8
Darren Cotterill <darren.cotterill@gmail.com>
2017-02-27 19:12:43 +1100

    Add new words

--- SpellingApp.py
+++ SpellingApp.py
@@ -1,213 +1,32 @@
 from bottle import route, run, request, template, static_file
-from os import system
+import os
 import random
 import csv
 
-word_meanings_week1 = {
-                 "yet":"I am yet to do my homework",
-                 "yes":"The opposite of no is yes",
-                 # "yell":"",
-                 "yells":"I dont like it when my brother yells at me",
-                 "yelled":"I yelled at the dog",
-                 # "yelling":"",
-                 "you":"you can do it",
-                 "yolk":"The egg has a white and a yolk",
-                 "yoyo":"The yoyo bounced up and down",
-                 "yellow":"The egg yolk is yellow",
-                 "year":"A year is 365 days",
-                 "yard":"I played in the back yard",
-                 "yawn":"Im so tired I want to yawn",
-                 "your":"These are your spelling words",
-                 "choose":"You can choose what you want to eat for dinner",
-                 "huge":"The earth is huge",
-                 "human":"Aliens are not human",
-                 "yearly":"Your birthday is yearly. Once a year",
-                 "yesterday":"The day before today is yesterday",
-                 "yoga":"I stretched in my yoga class",
-                 "young":"The opposite of old is young",
-                 # "yours":"",
-                 "yourself":"Try to be yourself",
-                 "youngest":"I am the youngest in the family",
-                 "usually":"I usually have toast for breakfast"
-}
-word_meanings_week2 = {
-                 "do":"what did you do today",
-                 "to":"I went to bed",
-                 "into":"go into the kitchen",
-                 "who":"who are you?",
-                 "two":"one plus one is two",
-                 "boot":"I put on my boot",
-                 "moon":"the moon is shining in the sky",
-                 "room":"I am in my room",
-                 "too":"there are too many carrots",
-                 "use":"I use a spoon to eat my pudding",
-                 "noon":"mid-day is called noon",
-                 "knew":"I wish I knew how to spell knew",
-                 "flew":"the bird flew into the sky",
-                 "blew":"The wind blew and blew",
-                 "broom":"I swept the floor with the broom",
-                 "classroom":"the teacher was in the classroom",
-                 "used":"I used my toys",
-                 "using":"I am using my toys",
-                 "juice":"I drank my delicious juice",
-                 "grew":"the plants grew tall",
-                 "threw":"I threw the ball",
-                 "through":"I walked through the door",
-                 #Not on the sheet, but in the homework
-                 "new":"I bought some new shoes",
-                 "food":"i ate all the food",
-                 "cool":"you are cool",
-                 "you":"you can do it",
-                 "Tuesday":"It is Tuesday today"}
-
-word_meanings_week3 = {
-                 "is":"What is your name",
-                 "his":"His name is Dillon",
-                 "as":"As nice as pie",
-                 "was":"I was on holiday",
-                 "zoom":"The fast car went zoom",
-                 "quiz":"We answered the questions in the quiz",
-                 "prize":"You won first prize",
-                 "zebra":"A zebra has black and white stripes",
-                 "fizz":"The cola had a lot of fizz",
-                 "buzz":"The bee went buzz",
-                 "buzzing":"The bee was buzzing",
-                 "close":"Please close the door",
-                 "closing":"The gate kept closing",
-                 "goes":"The car goes fast",
-                 "please":"Please help me",
-                 "present":"Here is your birthday present",
-                 "quizzes":"At school we do lots of quizzes",
-                 "sneeze":"Pepper can make you sneeze",
-                 "sizzle":"The sausage went sizzle on the barbecue"
-}
-
-word_meanings_week4 = {
-                "down": "I jumped up and down",
-                "about": "I am walking about",
-                "town": "I went shopping in the town",
-                "round": "The circle is round",
-                "count": "I can count to 100",
-                "cloud": "There is a cloud in the sky",
-                "clown": "A clown at the circus was funny",
-                "loud": "There was a loud bang",
-                "shout": "I can shout really loudly",
-                "sound": "I can hear the sound",
-                "how": "How about we do our homework",
-                "now": "I am doing my homework now",
-                "our": "Lets do our reading",
-                "out": "Shall we go out?",
-                "amount": "What amount of money you have",
-                "flower": "The flower was pretty",
-                "frown": "My face has a frown right now",
-                "crown": "The queen wore a crown",
-                "mountain": "I climbed the mountain",
-                "mouth": "I smile with my mouth",
-                "towel": "I dried myself with the towel",
-                "cow": "The cow went moo",
-                "ground": "I sat on the ground",
-                "thousand": "I have 1000 dollars"
-}
-
-word_meanings_week5 = {
-                "she":"she is a girl",
-                "ship":"I am on a ship",
-                "beach":"I am at the beach",
-                "lunch":"I am eating lunch",
-                "shelf":"The book is on the shelf",
-                "sound":"I can hear the sound",
-                "chip":"I am eating a chip",
-                "much":"I dont have much money",
-                "chain":"There is a chain around my neck",
-                "chew":"I must chew my food",
-                "catch":"I can catch a ball",
-                "chop":"I can chop food",
-                "shell":"I found a shell at the beach",
-                "church":"I got married in a church",
-                "wish":"I have a wish",
-                "chases":"The dog chases the cat",
-                "cheese":"I am eating cheese",
-                "itch":"I have an itch on my leg",
-                "kitchen":"I cook in the kitchen",
-                "shiver":"When I am cold I shiver",
-                "short":"The opposite of long is short",
-                "rubbish":"The bin is full of rubbish"
-                }
-
-word_meanings_week6 = {
-                "toy": "I played with the toy",
-                "boy": "Dillon is a boy",
-                "oil": "You need oil in your car",
-                "coin": "I flipped the coin",
-                "join": "You can join lego pieces together",
-                "ear": "Mr Bean can wiggle his ear",
-                "year": "Happy new year",
-                "toys": "I bought toys from the shop",
-                "here": "I live here",
-                "fear": "I fear zombies",
-                "near": "The boy is near the truck",
-                "dear": "I wrote a letter to my dear friend",
-                "deer": "There are lots of deer in this park",
-                "annoy": "You annoy me",
-                "boil": "The water is on the boil",
-                "enjoy": "I enjoy playing Pokemon cards",
-                "destroy": "I will destroy the world",
-                "noise": "Shouting makes a lot of noise",
-                "point": "You can point with your finger",
-                "beard": "I have a long beard",
-                "feared": "I have always feared zombies",
-                "clear": "The sky is clear",
-                "cheering": "I am cheering my friend at the soccer game"}
-
-word_meanings_week7 = {
-                # "the": "",
-                # "thin": "",
-                # "with": "",
-                "bath": "I was dirty so had a bath",
-                "path": "I walked down the path",
-                # "that": "",
-                # "thick": "",
-                # "them": "",
-                # "then": "",
-                "three": "one plus two is three",
-                # "these": "",
-                "birthday": "We sang happy birthday",
-                "month": "January is the first month of the year",
-                "mouth": "my mouth is open",
-                "teeth": "I bite with my teeth",
-                # "thank": "",
-                "third": "i came third in the race",
-                "thirsty": "i was thirsty so I drank some water",
-                # "gather": "",
-                "feather": "as light as a feather"
-                }
-
-word_meanings_week8 = {
-                "air": "",
-                "pair": "",
-                "hair": "",
-                "chair": "",
-                "where": "",
-                "their": "",
-                "stairs": "",
-                "bare": "",
-                "bear": "",
-                "dare": "",
-                "fair": "",
-                "repair": "",
-                "anywhere": "",
-                "square": "",
-                "careful": "",
-                "downstairs": "",
-                "scare": ""
-                }
-all_word_meanings = dict(word_meanings_week7.items()
-                         + word_meanings_week6.items()
-                         + word_meanings_week5.items()
-                         + word_meanings_week4.items()
-                         + word_meanings_week3.items()
-                         + word_meanings_week2.items())
-all_words = all_word_meanings.keys()
+word_meanings_week9 = {"ever":"",
+                       "over":"",
+                       "dollar":"",
+                       "other":"",
+                       "better":"",
+                       "dinner":"",
+                       "mother":"",
+                       "father":"",
+                       "caterpillar":"",
+                       "doctor":"",
+                       "butter":"",
+                       "letter":"",
+                       "actor":"",
+                       "under":"",
+                       "over":"",
+                       "paper":"",
+                       "corner":"",
+                       "December":"",
+                       "brother":"",
+                       "butterfly":"",
+                       "November":"",
+                       }
+all_word_meanings = {}
+all_words = {}
 
 
 person_name = ""
@@ -243,7 +62,7 @@ def find_first_wrong_letter(word, guess):
 
 @route('/spelling')
 def spelling():
-    return template ('welcome')
+    return template ('templates/welcome')
 
 @route('/spelling', method='POST')
 def do_spelling():
@@ -262,7 +81,7 @@ def do_spelling():
 
     create_correct_lists()
 
-    return template("spelling", person_name=person_name, correct_number=str(len(correct_list[correct_threshold])))
+    return template("templates/spelling", person_name=person_name, correct_number=str(len(correct_list[correct_threshold])))
 
 
 @route('/spell-word')
@@ -281,7 +100,8 @@ def spell_word():
 
     percent = 100 * len(correct_list[correct_threshold]) / len(all_words)
 
-    return template("spell-word", word_speech=word_speech, word_blanks=word_blanks, correct_number = str(len(correct_list[correct_threshold])),
+    return template("templates/spell-word", word_speech=word_speech, word_blanks=word_blanks,
+                                correct_number = str(len(correct_list[correct_threshold])),
                                 total_words = str(len(all_words)), progress_message = progress_message,
                                 percent = str(int(percent)))
 
@@ -296,7 +116,7 @@ def do_spell_word():
         # system("say " + message)
         correct_count[word] = 0
 
-        return template("wrong_length", length=str(len(word)), message=message)
+        return template("templates/wrong_length", length=str(len(word)), message=message)
 
     if (guess == word):
         if (word_blanks == "_ " * len(word)):  #there's not been a hint
@@ -305,11 +125,11 @@ def do_spell_word():
         for key, value in correct_count.items():
             writer.writerow([key, value])
 
-        return template("correct")
+        return template("templates/correct")
     else:
         correct_count[word] = 0
 
-        return template("wrong_spelling")
+        return template("templates/wrong_spelling")
 
 @route('/try-again', method = 'POST')
 def do_try_again():
@@ -331,7 +151,8 @@ def do_try_again():
 
     percent = 100 * len(correct_list[correct_threshold]) / len(all_words)
 
-    return template("spell-word", word_speech=word_speech, word_blanks=word_blanks, correct_number = str(len(correct_list[correct_threshold])),
+    return template("templates/spell-word", word_speech=word_speech, word_blanks=word_blanks,
+                                correct_number = str(len(correct_list[correct_threshold])),
                                 total_words = str(len(all_words)), progress_message = progress_message,
                                 percent = str(int(percent)))
 @route('/css/<filename>')
@@ -346,5 +167,31 @@ def server_static(filename):
 def server_static(filename):
     return static_file(filename, root='./fonts/')
 
+def load_words_and_definitions():
+    word_meanings = {}
+    try:
+        for file in os.listdir("words"):
+            if file.endswith(".csv"):
+                with open("words/" + file, 'r') as words:
+                    reader = csv.reader(words)
+                    print file
+                    word_meanings[file] = dict((rows[0],rows[1]) for rows in reader)
+    except IOError:
+        word_meanings = {}
+
+    print word_meanings
+    global all_word_meanings
+    print "----"
+    for words in word_meanings.values():
+        print words
+        all_word_meanings.update(words)
+
+    global all_words
+    all_words = all_word_meanings.keys()
+
+
+## MAIN
+
+load_words_and_definitions()
 run(host='0.0.0.0', port=8080, debug=True)
 
